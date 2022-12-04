48deaf25f9620d7224c0afd49742d54e37e3aca3
Megabyte-143 <yashygkf@gmail.com>
2022-12-04 09:45:12 +0530

    added corporate contract functions here

--- server/src/controllers/company.controller.ts
+++ server/src/controllers/company.controller.ts
@@ -5,85 +5,138 @@ import { sendNotification } from "../services/epns.service";
 import { getEmployeData } from "../services/lighthouse.service";
 import { fetchMyNfts } from "../services/revise.service";
 
+import { ethers } from "ethers";
+import provider from "../utils/ethersConnect";
+import { CorporateAddress } from "../contracts/addresses";
+import Coporate from "../contracts/abi/Corporate.json";
 
-export const getAllEmployees = asyncWrap(async (req: Request, res: Response, next: NextFunction) => {
+const corporate = new ethers.Contract(CorporateAddress, Coporate.abi, provider);
+
+export const getAllEmployees = asyncWrap(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const employees = await getEmployeData("0xa60f738a60BCA515Ac529b7335EC7CB2eE3891d2")
-        res.status(200).json(employees);
+      const employees = await getEmployeData(
+        "0xa60f738a60BCA515Ac529b7335EC7CB2eE3891d2"
+      );
+      res.status(200).json(employees);
     } catch (error) {
-        console.error(error);
-        throwError(400, `${error}`);
+      console.error(error);
+      throwError(400, `${error}`);
     }
-});
+  }
+);
 
-
-export const getCurrentBalance = asyncWrap(async (req: Request, res: Response, next: NextFunction) => {
+export const getCurrentBalance = asyncWrap(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).json({ success: false });
+      const balance = await corporate.getBalance();
+      res.status(200).json({ success: false });
     } catch (error) {
-        console.error(error);
-        throwError(400, `${error}`);
+      console.error(error);
+      throwError(400, `${error}`);
     }
-});
+  }
+);
 
-
-export const nextPayoutBalance = asyncWrap(async (req: Request, res: Response, next: NextFunction) => {
+export const nextPayoutBalance = asyncWrap(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).json({ success: false });
+      const balance = await corporate.getAmountToBePaid(30);
+      res.status(200).json({ success: false });
     } catch (error) {
-        console.error(error);
-        throwError(400, `${error}`);
+      console.error(error);
+      throwError(400, `${error}`);
     }
-});
+  }
+);
 
-
-export const withdrawableAmount = asyncWrap(async (req: Request, res: Response, next: NextFunction) => {
+export const withdrawableAmount = asyncWrap(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).json({ success: false });
+      const balance = await corporate.getWithdrawableAmount();
+      res.status(200).json({ success: false });
     } catch (error) {
-        console.error(error);
-        throwError(400, `${error}`);
+      console.error(error);
+      throwError(400, `${error}`);
     }
-});
+  }
+);
 
-
-export const postAddEmployee = asyncWrap(async (req: Request, res: Response, next: NextFunction) => {
+export const postAddEmployee = asyncWrap(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        // notification to employeee
-        await sendNotification({ msgTitle: "Added to team", msgBody: "You have been addeed to the team." }, "") // add address
-        res.status(200).json({ success: false });
+      const { name, walletAddress, payPeriod, payAmount, startTime } = req.body;
+
+      const tx = await corporate.addEmployee(
+        name,
+        walletAddress,
+        payPeriod,
+        payAmount,
+        startTime
+      );
+      // notification to employeee
+      await sendNotification(
+        {
+          msgTitle: "Added to team",
+          msgBody: "You have been addeed to the team.",
+        },
+        ""
+      ); // add address
+      res.status(200).json({ success: false });
     } catch (error) {
-        console.error(error);
-        throwError(400, `${error}`);
+      console.error(error);
+      throwError(400, `${error}`);
     }
-});
+  }
+);
 
-
-export const getMyNfts = asyncWrap(async (req: Request, res: Response, next: NextFunction) => {
+export const getMyNfts = asyncWrap(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const nfts = await fetchMyNfts()
-        res.status(200).json(nfts);
+      const nfts = await fetchMyNfts();
+      res.status(200).json(nfts);
     } catch (error) {
-        console.error(error);
-        throwError(400, `${error}`);
+      console.error(error);
+      throwError(400, `${error}`);
     }
-});
+  }
+);
 
-
-export const verifyInvoiceForFreelancer = asyncWrap(async (req: Request<{}, {}, { isVerified: boolean }>, res: Response, next: NextFunction) => {
+export const verifyInvoiceForFreelancer = asyncWrap(
+  async (
+    req: Request<{}, {}, { isVerified: boolean }>,
+    res: Response,
+    next: NextFunction
+  ) => {
     const { isVerified } = req.body;
     try {
-        if (!isVerified) {
-            await sendNotification({ msgTitle: "Invoice Rejected", msgBody: "Your Invoice have been rejected please talk to your employer" }, "") // add address of the employer
-            res.status(201).json({ message: "invoice have been rejected" })
-        }
-        // contact the smart contract here
-        await sendNotification({ msgTitle: "Invoice Accepted", msgBody: "Yay!!!... Your Invoice have been accepted." }, "") // add address of the employer
-        res.status(201).json({ message: "invoice have been accepted" })
-        const employees = await getEmployeData("0xa60f738a60BCA515Ac529b7335EC7CB2eE3891d2")
-        res.status(200).json(employees);
+      if (!isVerified) {
+        await sendNotification(
+          {
+            msgTitle: "Invoice Rejected",
+            msgBody:
+              "Your Invoice have been rejected please talk to your employer",
+          },
+          ""
+        ); // add address of the employer
+        res.status(201).json({ message: "invoice have been rejected" });
+      }
+      // contact the smart contract here
+      await sendNotification(
+        {
+          msgTitle: "Invoice Accepted",
+          msgBody: "Yay!!!... Your Invoice have been accepted.",
+        },
+        ""
+      ); // add address of the employer
+      res.status(201).json({ message: "invoice have been accepted" });
+      const employees = await getEmployeData(
+        "0xa60f738a60sBCA515Ac529b7335EC7CB2eE3891d2"
+      );
+      res.status(200).json(employees);
     } catch (error) {
-        console.error(error);
-        throwError(400, `${error}`);
+      console.error(error);
+      throwError(400, `${error}`);
     }
-});
+  }
+);
