38037c4528f30f0c63f33ff2acef0992ac4ab431
Megabyte-143 <yashygkf@gmail.com>
2022-12-04 09:51:11 +0530

    made a default provider in etheres

--- server/src/utils/ethersConnect.ts
+++ server/src/utils/ethersConnect.ts
@@ -1,6 +1,8 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
+const RPCUrl =
+  "https://convincing-thrilling-diamond.matic-testnet.discover.quiknode.pro/e4204f0263646fec0bccfe794fff845d27de2c5c/";
 
-const RPCUrl = "https://convincing-thrilling-diamond.matic-testnet.discover.quiknode.pro/e4204f0263646fec0bccfe794fff845d27de2c5c/"
+const provider = new ethers.providers.JsonRpcProvider(RPCUrl);
 
-const provider = new ethers.providers.JsonRpcProvider(RPCUrl)
+export default provider;
